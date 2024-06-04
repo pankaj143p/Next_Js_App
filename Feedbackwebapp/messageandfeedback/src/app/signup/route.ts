3c5a7efcd1aaba23c3dc9496a476de2722974027
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-06-04 14:23:30 +0530

    signup complete

--- Feedbackwebapp/messageandfeedback/src/app/signup/route.ts
+++ Feedbackwebapp/messageandfeedback/src/app/signup/route.ts
@@ -29,10 +29,28 @@ export async function POST(request : Request){
         const existingUserVerifiedByEmail = await UserModel.findOne({
             email
         })
+        // random otp generate 
         const verifycode = Math.floor(100000 + Math.random() * 900000).toString()
         
         if(existingUserVerifiedByEmail){
-            true 
+            if(existingUserVerifiedByEmail.isVerified){
+                return Response.json(
+                    {
+                        success: false,
+                        message: "Email already exists with user",
+                    },
+                    {
+                        status: 400,
+                    }
+                )
+            }else{
+               const hashPassword = await bcrypt.hash(password,10)
+               existingUserVerifiedByEmail.password = hashPassword;
+               existingUserVerifiedByEmail.verifyCode = verifycode;
+               existingUserVerifiedByEmail.verifyCodeExpiry=new Date(Date.now()+3600000)
+               await existingUserVerifiedByEmail.save() 
+            }
+
             
         }else{
             const hashPassword = await bcrypt.hash(password,10)
@@ -51,11 +69,34 @@ export async function POST(request : Request){
                 messages:  [],
             })
             await newUser.save()
-            await sendVerificationEmail(email,username,verifycode)
             
         }
+        // send verification code 
+      const emailResponse =   await sendVerificationEmail(email,username,verifycode)
+      
+        if(!emailResponse.success){
+            return Response.json(
+                {
+                    success: false,
+                    message: emailResponse.message,
+                },
+                {
+                    status: 500,
+                }
+            )
+        }
+        return Response.json(
+            {
+                success: true,
+                message: "User created successfully , please verify your email",
+            },
+            {
+                status: 201,
+            })
+        }     
+        
 
-    } catch (error){
+    catch (error){
         console.error('Error Registering User',error)
         return Response.json(
           {
